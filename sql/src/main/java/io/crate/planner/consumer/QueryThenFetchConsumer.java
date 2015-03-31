231acc19c7f8e602aa4e68322e697c9586b49037
Sebastian Utz <su@rtme.net>
2015-03-31 16:53:58 +0200

    wip

--- sql/src/main/java/io/crate/planner/consumer/QueryThenFetchConsumer.java
+++ sql/src/main/java/io/crate/planner/consumer/QueryThenFetchConsumer.java
@@ -95,11 +95,9 @@ public class QueryThenFetchConsumer implements Consumer {
                 return new NoopPlannedAnalyzedRelation(table);
             }
 
-            // TODO: if _score is selected, ordering by _score using a TopNProjection is only temporarily and MUST be changed.
-            // Proposal: only order by _score if requested by user and order it on the shard
             boolean scoreSelected = false;
             ReferenceInfo docIdRefInfo = tableInfo.getReferenceInfo(DOC_ID_COLUMN_IDENT);
-            List<Symbol> collectSymbols = Lists.<Symbol>newArrayList(new Reference(docIdRefInfo));
+            List<Symbol> collectSymbols = Lists.<Symbol>newArrayList();
 
             List<Symbol> outputSymbols = new ArrayList<>(table.querySpec().outputs().size());
             for (Symbol symbol : table.querySpec().outputs()) {
@@ -112,12 +110,17 @@ public class QueryThenFetchConsumer implements Consumer {
 
             List<Projection> collectProjections = new ArrayList<>();
             OrderBy orderBy = table.querySpec().orderBy();
+
+            boolean orderBySymbolNotInOutput = false;
             if (orderBy != null) {
                 table.tableRelation().validateOrderBy(orderBy);
                 for (Symbol symbol : orderBy.orderBySymbols()) {
                     if (!collectSymbols.contains(symbol)) {
                         // order by symbols will be resolved on collect
                         collectSymbols.add(symbol);
+                    }
+                    if (!orderBySymbolNotInOutput && !table.querySpec().outputs().contains(symbol)) {
+                        orderBySymbolNotInOutput = true;
                     }
                 }
 
@@ -129,6 +132,13 @@ public class QueryThenFetchConsumer implements Consumer {
                 collectProjections.add(mergeProjection);
             }
 
+            boolean needFetchPhase = false;
+            boolean keepContextForFetcher = false;
+            if (orderBySymbolNotInOutput) {
+                collectSymbols.add(0, new Reference(docIdRefInfo));
+                keepContextForFetcher = true;
+                needFetchPhase = true;
+            }
             CollectNode collectNode = PlanNodeBuilder.collect(
                     tableInfo,
                     context.plannerContext(),
@@ -138,35 +148,44 @@ public class QueryThenFetchConsumer implements Consumer {
                     orderBy,
                     MoreObjects.firstNonNull(table.querySpec().limit(), Constants.DEFAULT_SELECT_LIMIT) + table.querySpec().offset()
             );
-            collectNode.keepContextForFetcher(true);
+            collectNode.keepContextForFetcher(keepContextForFetcher);
             collectNode.projections(collectProjections);
 
-            List<Projection> mergeProjections = new ArrayList<>(2);
+            List<Projection> mergeProjections = new ArrayList<>();
             TopNProjection topNProjection = new TopNProjection(
                     MoreObjects.firstNonNull(table.querySpec().limit(), Constants.DEFAULT_SELECT_LIMIT),
                     table.querySpec().offset()
             );
-            List<Symbol> outputs = new ArrayList<>(2);
-            outputs.add(DEFAULT_DOC_ID_INPUT_COLUMN);
-            if (scoreSelected) {
-                outputs.add(DEFAULT_SCORE_INPUT_COLUMN);
-            }
-            topNProjection.outputs(outputs);
             mergeProjections.add(topNProjection);
 
-            // by default don't split fetch requests into pages/chunks,
-            // only if record set is higher than default limit
-            int bulkSize = FetchProjector.NO_BULK_REQUESTS;
-            if (topNProjection.limit() > Constants.DEFAULT_SELECT_LIMIT) {
-                bulkSize = Constants.DEFAULT_SELECT_LIMIT;
+            // TODO: if no dedicated fetchPhase is needed we should stick to QAF instead
+            if (needFetchPhase) {
+                List<Symbol> outputs = new ArrayList<>(2);
+                outputs.add(DEFAULT_DOC_ID_INPUT_COLUMN);
+                if (scoreSelected) {
+                    outputs.add(DEFAULT_SCORE_INPUT_COLUMN);
+                }
+                topNProjection.outputs(outputs);
+                // by default don't split fetch requests into pages/chunks,
+                // only if record set is higher than default limit
+                int bulkSize = FetchProjector.NO_BULK_REQUESTS;
+                if (topNProjection.limit() > Constants.DEFAULT_SELECT_LIMIT) {
+                    bulkSize = Constants.DEFAULT_SELECT_LIMIT;
+                }
+                FetchProjection fetchProjection = new FetchProjection(
+                        DEFAULT_DOC_ID_INPUT_COLUMN, collectSymbols, outputSymbols,
+                        tableInfo.partitionedByColumns(),
+                        collectNode.executionNodes(),
+                        bulkSize,
+                        table.querySpec().isLimited());
+                mergeProjections.add(fetchProjection);
+            } else {
+                List<Symbol> outputs = new ArrayList<>(outputSymbols.size());
+                for (int i = 0; i < outputSymbols.size(); i++) {
+                    outputs.add(new InputColumn(i, outputSymbols.get(i).valueType()));
+                }
+                topNProjection.outputs(outputs);
             }
-            FetchProjection fetchProjection = new FetchProjection(
-                    DEFAULT_DOC_ID_INPUT_COLUMN, collectSymbols, outputSymbols,
-                    tableInfo.partitionedByColumns(),
-                    collectNode.executionNodes(),
-                    bulkSize,
-                    table.querySpec().isLimited());
-            mergeProjections.add(fetchProjection);
 
             MergeNode localMergeNode;
             if (orderBy != null) {
